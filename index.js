10d5dfe044fb3d12a13d564b83b99a132f818b7a
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-18 22:19:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ rl.close()
 } 
 
 setTimeout(async () => {
-let codeBot = await sock.requestPairingCode(addNumber)
+let codeBot = await kim.requestPairingCode(addNumber)
 codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
 console.log(chalk.bold.white(chalk.bgMagenta(`(●'▽ '●)ゝ 🩷  ᥴ᥆ძіg᥆ ძᥱ ᥎іᥒᥴᥙᥣᥲᥴі᥆ᥒ 🩷 : `)), chalk.bold.white(chalk.white(codeBot)))
 }, 2000)
@@ -108,11 +108,11 @@ mek = chatUpdate.messages[0]
 if (!mek.message) return
 mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 if (mek.key && mek.key.remoteJid === 'status@broadcast') return
-if (!sock.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+if (!kim.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
 if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
 if (mek.key.id.startsWith('FatihArridho_')) return
-global.numBot = sock.user.id.split(":")[0] + "@s.whatsapp.net"
-global.numBot2 = sock.user.id
+global.numBot = kim.user.id.split(":")[0] + "@s.whatsapp.net"
+global.numBot2 = kim.user.id
 m = smsg(kim, mek)
 require("./kim")(kim, m, chatUpdate, mek, store)
 } catch (e) {
