046d3f65a3b1be8d8796fb844f532a6a448a0b94
Ziooo <103476365+Alexito-Hub@users.noreply.github.com>
2024-06-18 21:14:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+import { DisconnectReason, makeWASocket, makeInMemoryStore, useMultiFileAuthState, makeCacheableSignalKeyStore, Browsers } from "@whiskeysockets/baileys";
+import { Boom } from "@hapi/boom";
+import pino from "pino";
 
+const { state, saveCreds } = await useMultiFileAuthState('auth');
+const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) })
+
+const start = async() => {
+    let kim = await makeWASocket({
+        logger: pino({ level: "silent" }),
+        browser: Browsers.ubuntu('Chrome'),
+        printQRInTerminal: true,
+        auth: {
+		    creds: state.creds,
+		    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "silent" })),
+		}
+    })
+    
+    store?.bind(kim.ev);
+    
+    kim.ev.on("connection.update", async({ connection, lastDisconnect}) => {
+        if (connection == "close") {
+            let reason = new Boom(lastDisconnect?.error)?.output?.statuscode
+            if (reason == DisconnectReason.connectionClose) { 
+                console.log(`Se cerro la conexion conectando de nuevo`);
+                start();
+            }
+        }
+        if (connection == "open") {
+			console.log(`Kim es online.`);
+		};
+    })
+}
+
+start()
