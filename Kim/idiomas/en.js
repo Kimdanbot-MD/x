52e79c5af95530e181dc3b0a8eb1fc77e514fe18
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-07-01 00:41:28 -0500

    Update en.js

--- Kim/idiomas/en.js
+++ Kim/idiomas/en.js
@@ -1,2 +1,4 @@
 const lenguaje = () => { return 'en' } 
-module.exports = {}
+
+const tmp = () => { return `┏━━━◉━━━━━⬤━━━━━⪩『 🍒   』⪨━━━━━⬤━━━━━◉━━┅┅\n┃ sᥱ ᥣіm⍴і᥆ ᥴ᥆rrᥱᥴ𝗍ᥲmᥱᥒ𝗍ᥱ ᥣᥲ ᥴᥲr⍴ᥱ𝗍ᥲ 𝗍ᥱm⍴ \n┗━━━◉━━━━━⬤━━━━━⪩『 🍒   』⪨━━━━━⬤━━━━━◉━━┅┅\n` } 
+module.exports = {tmp}
