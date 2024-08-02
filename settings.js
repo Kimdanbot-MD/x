6c91acdbcb224a37ae10621dd4a3b22793f54cdd
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-08-02 18:10:18 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 import chalk from "chalk"
 import path from 'path'
 import fs from "fs"
-import es from './Kim/idiomas/es.js'
-import en from './Kim/idiomas/en.js'
+const es = require('./Kim/idiomas/es.js') //Español 
+const en = require('./Kim/idiomas/en.js') //Ingles 
 
 global.owner = [
 ["573234628903", "creador", true],
