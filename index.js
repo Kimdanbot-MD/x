18147012140b9416e29f2f1a8732f13e704723a5
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-08-04 23:42:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import fs from "fs";
 import readline from "readline";
 import NodeCache from 'node-cache'
 import cfonts from 'cfonts'
-import { smsg, getBuffer, sleep} from './Kim/fuctions'
+import { smsg, getBuffer, sleep} from './Kim/fuctions.js'
 
 const methodCodeQR = process.argv.includes("qr")
 const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
