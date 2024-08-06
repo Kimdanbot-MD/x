81ae51de21508326e83ae3a27b49bc2beeeb4709
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-08-06 17:31:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ kim.ev.on('messages.upsert', async (chatUpdate) => {
         global.numBot = kim.user.id.split(":")[0] + "@s.whatsapp.net";
         global.numBot2 = kim.user.id;
         const m = smsg(kim, mek);
-        import {kim, m, chatUpdate, mek, store} from "./kim.js"
+        import { kim, m, chatUpdate, mek, store } from "./kim.js"
         } catch (e) {
         console.error(e)}}) 
       } catch (err) {
