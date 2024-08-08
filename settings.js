23210aa2fb7a008c87441249d9ca7096472ecc08
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-08-08 11:00:52 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ import fs from "fs"
 import es from './Kim/idiomas/es.js' //Español 
 import en from './Kim/idiomas/en.js' //Ingles 
 
-export default const owner = [
+export const owner = [
 ["573234628903", "creador", true],
 ["573044062173"], 
 ["50685690440"],
@@ -13,32 +13,32 @@ export default const owner = [
 ["51968374620", "Ale", true],
 ["522221496388", " suro", true]]
 
-export default const vip = [
+export const vip = [
 owner 
 ]
 
-export default const aport = [
+export const aport = [
   vip
 ]
 
 // ═════════════𓊈『 IDIOMAS 』𓊉═════════════ 
-export const default place = 'America/Bogota' 
-export default const lenguaje = es 
-export default const prefix = [`.`]
+export const place = 'America/Bogota' 
+export const lenguaje = es 
+export const prefix = [`.`]
 
 // ═════════════𓊈『 APIS 』𓊉═════════════ 
-export default const keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
-export default const keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
-export default const keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]; 
-export default const keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']; 
-export default const keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]; 
-export default const keysneoxrrr = ['5VC9rvNx', 'cfALv5']; 
-export default const keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]; 
-export default const lolkeysapi = ['GataDios']; // ['BrunoSobrino_2'] 
-export default const itsrose = ['4b146102c4d500809da9d1ff'];
-export default const API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '');
+export const keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
+export const keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
+export const keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]; 
+export const keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']; 
+export const keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]; 
+export const keysneoxrrr = ['5VC9rvNx', 'cfALv5']; 
+export const keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]; 
+export const lolkeysapi = ['GataDios']; // ['BrunoSobrino_2'] 
+export const itsrose = ['4b146102c4d500809da9d1ff'];
+export const API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '');
 
-export default const APIs = {
+export const APIs = {
 //ApiEmpire: 'https://',
 CFROSAPI: 'https://api.cafirexos.com',
 nrtm: 'https://fg-nrtm.ddns.net',
@@ -60,7 +60,7 @@ erdwpe: 'https://api.erdwpe.com',
 xyroinee: 'https://api.xyroinee.xyz',
 nekobot: 'https://nekobot.xyz'
 },
-export default const APIKeys = {
+export const APIKeys = {
 'https://api.xteam.xyz': `${keysxteam}`,
 'https://api.lolhuman.xyz': 'GataDios',
 'https://api.neoxr.my.id': `${keysneoxr}`,
@@ -75,29 +75,29 @@ export default const APIKeys = {
 };
 
 // ═════════════𓊈『 LINKS 』𓊉═════════════ 
-export default const  md = 'https://github.com/Kimdanbot-MD/KimdanBot-MD'
-export default const  yt = 'https://youtube.com/@universobl?si=HeFdv4RaYDF9HAeX'
-export default const  tiktok = 'https://www.tiktok.com/@universo_yaoi_bl?_t=8iIlNrlJg1d&_r=1'
-export default const  fb = 'https://www.instagram.com/_universo.bl?igshid=OGQ5ZDc2ODk2ZA=='
-export default const  red = [md, yt, tiktok, fb]
+export const  md = 'https://github.com/Kimdanbot-MD/KimdanBot-MD'
+export const  yt = 'https://youtube.com/@universobl?si=HeFdv4RaYDF9HAeX'
+export const  tiktok = 'https://www.tiktok.com/@universo_yaoi_bl?_t=8iIlNrlJg1d&_r=1'
+export const  fb = 'https://www.instagram.com/_universo.bl?igshid=OGQ5ZDc2ODk2ZA=='
+export const  red = [md, yt, tiktok, fb]
 
-export default const nna = 'https://whatsapp.com/channel/0029VaFFJab3QxS5sqmnXR3l' //canal
-export default const nna2 = 'https://whatsapp.com/channel/0029VaNCiOMFSAtAQVOwA50y' //canal
-export default const nna3 = 'https://whatsapp.com/channel/0029VaIJ2NSGE56k4PCzfd1E' //canal
-export default const nna4 = 'https://whatsapp.com/channel/0029VaFFJab3QxS5sqmnXR3l' //canal
+export const nna = 'https://whatsapp.com/channel/0029VaFFJab3QxS5sqmnXR3l' //canal
+export const nna2 = 'https://whatsapp.com/channel/0029VaNCiOMFSAtAQVOwA50y' //canal
+export const nna3 = 'https://whatsapp.com/channel/0029VaIJ2NSGE56k4PCzfd1E' //canal
+export const nna4 = 'https://whatsapp.com/channel/0029VaFFJab3QxS5sqmnXR3l' //canal
 
-export default const nn = 'https://chat.whatsapp.com/C0lYCnklEtg1HUkbR4uPxA' //kim
-export default const nn2 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn3 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn4 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn5 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn6 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn7 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn8 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn9 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const nn10 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
-export default const ca = [nna, nna2, nna3, nna4]
-export default const wa = [nna, nna2, nna3, nna4, nn, nn2, nn3, nn4, nn5, nn6, nn7, nn8, nn9, nn10]
+export const nn = 'https://chat.whatsapp.com/C0lYCnklEtg1HUkbR4uPxA' //kim
+export const nn2 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn3 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn4 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn5 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn6 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn7 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn8 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn9 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const nn10 = 'https://chat.whatsapp.com/Fj2edZ8XtV48tyNLZn3rdE' //kim2
+export const ca = [nna, nna2, nna3, nna4]
+export const wa = [nna, nna2, nna3, nna4, nn, nn2, nn3, nn4, nn5, nn6, nn7, nn8, nn9, nn10]
 
 // ═════════════𓊈『 GLOBAL 』𓊉════════════ 
 /*global.mess = {
@@ -134,19 +134,19 @@ global.error = '❌'
 global.xmoji = '🔥' */
 
 // ═════════════𓊈『 INFO 』𓊉═════════════ 
-export default const botname = "𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃"
-export default const wm = "                𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃"
-export default const packname = "🍓 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🍓"
-export default const author = "🍒𝐃𝐚𝐧𝐨𝐧𝐢𝐧𝐨🍒"
-export default const vs = '𝟏.𝟎.𝟎'
-export default const botNumberCode = "" //Ejemplo: +57
-export default const phoneNumber = ""
+export const botname = "𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃"
+export const wm = "                𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃"
+export const packname = "🍓 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🍓"
+export const author = "🍒𝐃𝐚𝐧𝐨𝐧𝐢𝐧𝐨🍒"
+export const vs = '𝟏.𝟎.𝟎'
+export const botNumberCode = "" //Ejemplo: +57
+export const phoneNumber = ""
 
 // ═════════════𓊈『 LISTAS 』𓊉═════════════ 
-export default const mods = []
-export default const premium = []  
-export default const blockList = []  
+export const mods = []
+export const premium = []  
+export const blockList = []  
 
 // ═════════════𓊈『 NIVELES 』𓊉═════════════ 
-export default const multiplier = 90 // Cuanto más alto, más difícil subir de nivel 
-export default const maxwarn = '4' // máxima advertencias  
+export const multiplier = 90 // Cuanto más alto, más difícil subir de nivel 
+export const maxwarn = '4' // máxima advertencias  
