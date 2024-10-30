59591b4c163f5206db915bae36f92a1efcff7f3b
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-10-30 17:22:23 -0500

    Update commandHandler.js

--- Kim/core/commandHandler.js
+++ Kim/core/commandHandler.js
@@ -14,11 +14,32 @@ export class CommandHandler {
           await m.reply(`Pong! ${Date.now() - startTime}ms`);
           break;
         default:
+          if (m.text.startsWith('>')) {
+            if (!isOwner) return m.reply(mess.owner);
+            try {
+              return m.reply(JSON.stringify(eval(m.text.slice(2)), null, '\t'));
+            } catch (e) {
+              return m.reply(String(e));
+            }}
+          if (m.text.startsWith('=>')) {
+            if (!isOwner) return m.reply(mess.owner);
+            try {
+              let result = await eval(`(async () => { ${m.text.slice(3)} })()`);
+              return m.reply(JSON.stringify(result, null, '\t'));
+            } catch (e) {
+              return m.reply(String(e));
+            }}
+          if (m.text.startsWith('$')) {
+            if (!isOwner) return m.reply(mess.owner);
+            try {
+              return m.reply(String(execSync(m.text.slice(2), { encoding: 'utf-8' })));
+            } catch (err) {
+              console.error(err);
+              return m.reply(String(err));
+            }}
           await m.reply('Comando no encontrado.');
       }
     } catch (error) {
       console.error('Error al ejecutar el comando:', error);
       await m.reply('Ha ocurrido un error al ejecutar el comando.');
-    }
-  }
-}
+    }}}
