18ed53de663a1d30c91e9217903c8dd8c1eba295
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-10-30 11:57:40 -0500

    Update and rename Kim/settings.js to config.js

--- config.js
+++ config.js
@@ -10,8 +10,7 @@ global.owner = [
 ["50685690440"],
 ["573148243189"],
 ["5214434703586", "Zam", true],
-["51968374620", "Ale", true],
-["522221496388", " suro", true]]
+["51968374620", "Ale", true]]
 
 global.vip = [
 owner 
@@ -25,53 +24,6 @@ global.aport = [
 global.place = 'America/Bogota' // Aquí puede encontrar su ubicación https://momentjs.com/timezone/
 global.lenguaje = es // Aquí puede encontrar su idioma https://cloud.google.com/translate/docs/languages?hl=es-419
 global.prefix = [`.`]
-
-// ═════════════𓊈『 APIS 』𓊉═════════════ 
-global.keysZens = ['LuOlangNgentot', 'c2459db922', '37CC845916', '6fb0eff124', 'hdiiofficial', 'fiktod', 'BF39D349845E', '675e34de8a', '0b917b905e6f']; 
-global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]; 
-global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']; 
-global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]; 
-global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']; 
-global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]; 
-global.lolkeysapi = ['GataDios']; // ['BrunoSobrino_2'] 
-global.itsrose = ['4b146102c4d500809da9d1ff'];
-global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({...query, ...(apikeyqueryname ? {[apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name]} : {})})) : '');
-
-global.APIs = {
-//ApiEmpire: 'https://',
-CFROSAPI: 'https://api.cafirexos.com',
-nrtm: 'https://fg-nrtm.ddns.net',
-fgmods: 'https://api.fgmods.xyz', 
-xteam: 'https://api.xteam.xyz',
-dzx: 'https://api.dhamzxploit.my.id',
-lol: 'https://api.lolhuman.xyz',
-neoxr: 'https://api.neoxr.my.id',
-zenzapis: 'https://api.zahwazein.xyz',
-akuari: 'https://api.akuari.my.id',
-akuari2: 'https://apimu.my.id',
-botcahx: 'https://api.botcahx.biz.id',
-ibeng: 'https://api.ibeng.tech/docs',
-rose: 'https://api.itsrose.site',
-popcat: 'https://api.popcat.xyz',
-xcoders: 'https://api-xcoders.site',
-vihangayt: 'https://vihangayt.me',
-erdwpe: 'https://api.erdwpe.com',
-xyroinee: 'https://api.xyroinee.xyz',
-nekobot: 'https://nekobot.xyz'
-},
-global.APIKeys = {
-'https://api.xteam.xyz': `${keysxteam}`,
-'https://api.lolhuman.xyz': 'GataDios',
-'https://api.neoxr.my.id': `${keysneoxr}`,
-'https://api.zahwazein.xyz': `${keysxxx}`,
-'https://api.fgmods.xyz': 'DRLg5kY7', 
-'https://api-fgmods.ddns.net': 'fg-dylux',
-'https://api.botcahx.biz.id': 'Admin',
-'https://api.ibeng.tech/docs': 'tamvan',
-'https://api.itsrose.site': 'Rs-Zeltoria',
-'https://api-xcoders.site': 'Frieren',
-'https://api.xyroinee.xyz': 'uwgflzFEh6'
-};
 
 // ═════════════𓊈『 LINKS 』𓊉═════════════ 
 global.md = 'https://github.com/Kimdanbot-MD/KimdanBot-MD'
