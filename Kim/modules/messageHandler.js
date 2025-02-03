9ba45a793a91af0f0afb16b62010bbff7bc1efd8
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2025-02-02 23:11:59 -0500

    Update messageHandler.js

--- Kim/modules/messageHandler.js
+++ Kim/modules/messageHandler.js
@@ -1,8 +1,8 @@
-import { getGroupAdmins, isUrl, sleep, generateProfilePicture, parseMention, getRandom } from "../Kim/utils/fuctions.js";
+import { getGroupAdmins, isUrl, sleep, generateProfilePicture, parseMention, getRandom } from "../utils/fuctions.js";
 import moment from 'moment-timezone';
 import chalk from "chalk";
 import gradient from 'gradient-string';
-import { CommandHandler } from "../Kim/core/CommandHandler.js";
+import { CommandHandler } from "../core/CommandHandler.js";
 
 export class MessageHandler {
   constructor(kim, store) {
