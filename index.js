7e2f07251ce737380a97022741ba04798e23edfe
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2025-02-03 01:34:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,33 +9,34 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 const question = (text) => new Promise((resolve) => rl.question(text, resolve))
 
 const start = async () => {
-  try {
-    const authHandler = new AuthHandler();
-    const opcion = await authHandler.obtenerOpcionConexion();
+    try {
+        const authHandler = new AuthHandler();
+        const opcion = await authHandler.obtenerOpcionConexion(); // Obtiene la opción
 
-    const baileysConnection = new BaileysConnection({
-      authPath: 'auth',
-      printQRInTerminal: opcion === '1',
-      browser: opcion === '1' ? ['KimdanBot-MD', 'Safari', '1.0.0'] : ["Ubuntu", "Edge", "20.0.04"],
-      version: (await fetchLatestBaileysVersion()).version,
-    });
+        const baileysConnection = new BaileysConnection({
+            authPath: 'auth',
+            printQRInTerminal: opcion === '1', // Usa la opción para QR
+            browser: opcion === '1' ? ['KimdanBot-MD', 'Safari', '1.0.0'] : ["Ubuntu", "Edge", "20.0.04"],
+            version: (await fetchLatestBaileysVersion()).version,
+            opcion: opcion // Pasa la opción a BaileysConnection
+        });
 
-    const { kim, store } = await baileysConnection.connect();
-    const { saveCreds } = await useMultiFileAuthState('auth');
-    const eventHandler = new EventHandler(kim, store, start);
-    const connectionHandler = new ConnectionHandler(kim, store, start);
+        const { kim, store } = await baileysConnection.connect(); // Llama a connect() después de pasar la opción
+        const { saveCreds } = await useMultiFileAuthState('auth');
+        const eventHandler = new EventHandler(kim, store, start);
+        const connectionHandler = new ConnectionHandler(kim, store, start);
 
-    kim.ev.on('connection.update', connectionHandler.onConnectionUpdate);
-    kim.ev.on('messages.upsert', eventHandler.onMessageUpsert);
-    kim.ev.on('messages.update', eventHandler.onMessagesUpdate);
-    kim.ev.on('group-participants.update', eventHandler.onGroupParticipantsUpdate);
+        kim.ev.on('connection.update', connectionHandler.onConnectionUpdate);
+        kim.ev.on('messages.upsert', eventHandler.onMessageUpsert);
+        kim.ev.on('messages.update', eventHandler.onMessagesUpdate);
+        kim.ev.on('group-participants.update', eventHandler.onGroupParticipantsUpdate);
 
-    store?.bind(kim.ev);
-    kim.ev.on('creds.update', saveCreds);
+        store?.bind(kim.ev);
+        kim.ev.on('creds.update', saveCreds);
 
-  } catch (error) {
-    console.error('Error al iniciar el bot:', error);
-  }
+    } catch (error) {
+        console.error('Error al iniciar el bot:', error);
+    }
 };
 
 start();
